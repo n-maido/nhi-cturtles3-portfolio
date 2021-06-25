2651ec28dac6c311341b52e11127a1191b9b9ecd
Cloud User <centos@ip-172-31-28-231.us-west-1.compute.internal>
2021-06-25 02:29:16 +0000

    bash script to test endpoints

---
+++ portfolio_testing.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 3.03 ex3
+# Objective: Test every endpoint of portfolio and print the success/error msgs
+
+function test_endpts(){
+	test_data="username=$1&password=$2"
+	portfolio_url="https://nhi-hawksbills.duckdns.org"
+
+	# test health
+	echo "Testing /health..."
+    status_code=$(curl -s -o response.txt -w "%{http_code}" "$portfolio_url/health")
+    handle_response $status_code
+
+    # test register-GET
+    printf "Testing /register...\n"
+    status_code=$(curl -s -o response.txt -w "%{http_code}" "$portfolio_url/register")
+    handle_response $status_code
+
+    # test register-POST
+    status_code=$(curl -s -o response.txt -w "%{http_code}" -X POST -d "$test_data" "$portfolio_url/register")
+    handle_response $status_code
+
+
+    # test login-GET
+    printf "\nTesting /login...\n"
+    status_code=$(curl -s -o response.txt -w "%{http_code}" "$portfolio_url/register")
+    handle_response $status_code
+
+    # test login-POST
+    status_code=$(curl -s -o response.txt -w "%{http_code}" -X POST -d "$test_data" "$portfolio_url/login")
+    handle_response $status_code
+}
+
+function handle_response(){
+    status_code=$1
+
+    if [[ $status_code -eq 200 || -z $status_code ]]; then
+        printf "SUCCESS\n"
+    else
+        printf "ERROR: Request returned status code $status_code\n"
+    fi
+    
+}
