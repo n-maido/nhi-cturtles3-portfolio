94e4d8a674bc87bd71cc16454fd84f5b20fda777
Lint Action <lint-action@samuelmeuli.com>
2021-07-09 20:54:22 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-#from flask import Flask, render_template, url_for, request, redirect, json
+# from flask import Flask, render_template, url_for, request, redirect, json
 from flask import Flask, render_template, request, json
 import os
 import json
